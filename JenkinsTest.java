49a822fbd7e116c761042c035544d9257e4f1e70
2019wa86550@wilp.bits-pilani.ac.in <lakshaysaxena8@gmail.com>
2024-08-01 00:00:00 +0530

    Initial commit

---
+++ JenkinsTest.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class JenkinsTest {
+    public static void main(String[] args) {
+        System.out.println("hey there");
+        List<String> list = Arrays.asList("Apple", "Banana", "Cherry");
+        System.out.println(list);
+        System.out.println("hey again");
+
+    }
+}
