65b8640a831e751a4d0af29b300edac3f3c73afe
2019wa86550@wilp.bits-pilani.ac.in <lakshaysaxena8@gmail.com>
2024-08-01 00:20:14 +0530

    Added stream filter starts with A
    Added stream filter starts with A

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class JenkinsTest {
     public static void main(String[] args) {
@@ -8,6 +10,9 @@ public class JenkinsTest {
         System.out.println(list);
         System.out.println("hey again");
         System.out.println("lets go");
+        List<String> list1 = list.stream().filter(i -> i.startsWith("A")).collect(Collectors.toList());
+        System.out.println(" Element starting with A");
+        System.out.println(list1);
 
     }
 }
